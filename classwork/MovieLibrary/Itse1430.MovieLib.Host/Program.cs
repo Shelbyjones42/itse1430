c57c4d4c80fb693dc2f2f29d458b806d3adc89a6
Shelby.jones664 <Shelby.jones664@NEITD-211215>
2019-09-11 18:17:07 -0500

    todays class

--- classwork/MovieLibrary/Itse1430.MovieLib.Host/Program.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib.Host/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Itse1430.MovieLib.Host
@@ -16,7 +13,7 @@ namespace Itse1430.MovieLib.Host
         {
             Application.EnableVisualStyles ();
             Application.SetCompatibleTextRenderingDefault (false);
-            Application.Run (new Form1 ());
+            Application.Run (new MainForm ());
         }
     }
 }
