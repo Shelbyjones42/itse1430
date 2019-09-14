35eead051cc99b9cbdf106538faf0f2748b036c1
Shelby Jones <shelby.jones664@my.tccd.edu>
2019-09-14 00:44:22 -0500

    Today's work

--- labs/Itse1430.Maze/Itse1430.Maze/Program.cs
+++ labs/Itse1430.Maze/Itse1430.Maze/Program.cs
@@ -11,9 +11,68 @@ namespace Itse1430.Maze
 
         static void Main ( string[] args )
         {
+            Console.WriteLine("You are about to enter the Maze of Colors.");
+            Console.ReadLine ();
+        }
+        private static void Room1()
+        {
+            
+        }
+
+        private static void Room2()
+        {
 
         }
 
+        private static void Room3 ()
+        {
+
+        }
+
+        private static void Room4 ()
+        {
+
+        }
+
+        private static void Room5 ()
+        {
+
+        }
+
+        private static void Room6 ()
+        {
+
+        }
+
+        private static void Room7 ()
+        {
+
+        }
+
+        private static void Room8 ()
+        {
+
+        }
+
+        private static void Room9 ()
+        {
+
+        }
+
+        private static void Room10 ()
+        {
+
+        }
+
+        private static void Room11 ()
+        {
+
+        }
+
+        private static void Room12 ()
+        {
+
+        }
 
     }
 }
