44ae010a58dec50c1a05cabbbea29a150566ac55
Shelby.jones664 <Shelby.jones664@NEITD-211215>
2019-09-16 19:10:01 -0500

    Today's Work

--- classwork/MovieLibrary/Itse1430.MovieLib/Movie.cs
+++ classwork/MovieLibrary/Itse1430.MovieLib/Movie.cs
@@ -6,7 +6,21 @@ using System.Threading.Tasks;
 
 namespace Itse1430.MovieLib
 {
+    /// <summary>Represents movie data.</summary>
     public class Movie
     {
-    }
-}
+        // Fields - data to be stored
+        // TODO: Never make fields public!!
+        public string title = "";
+        public string description = "";
+        public int realeaseYear = 1900;
+        public string rating = "";
+        public bool hasSeen;
+        public int runLength;
+
+        // Can new up other objects
+        // Can't use with the fields above 
+        //private Movie orginalMovie = new Movie ();
+    }   
+}       
+           
